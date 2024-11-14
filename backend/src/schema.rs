872b69f0c7fac89f1dad2716847507d8b1e17fde
Adam Olsson <dagadamsven@gmail.com>
2024-11-14 17:22:44 +0100

    Undo last 2 commits

--- backend/src/schema.rs
+++
@@ -1,9 +0,0 @@
-diesel::table! {
-    slides (id) {
-        id -> Integer,
-        title -> Text,
-        content -> Text,
-        start_date -> Timestamp,
-        end_date -> Timestamp,
-    }
-}
