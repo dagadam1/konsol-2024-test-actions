d3ed7d8c79b3eeadeb0d04f785d9ed2d13aca6b2
Adam Olsson <dagadamsven@gmail.com>
2025-03-04 16:06:59 +0100

    Add images to mock data script

--- backend/scripts/mock_data.py
+++ backend/scripts/mock_data.py
@@ -2,24 +2,40 @@ import sqlite3
 import uuid
 import random
 from datetime import datetime, timedelta
+import requests
+from dotenv import load_dotenv
+import sys
+import os
+
+load_dotenv()
 
 def create_mock_slides(db_path: str, count: int):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     
-    filetypes = ["jpg", "png", "gif", "mp4"]
     now = datetime.utcnow()
     
     slides = []
     for i in range(count):
         slide_id = str(uuid.uuid4())
+        
+        try:
+            image_path = f"../{os.environ['IMAGE_PATH']}/{slide_id}.jpg"
+        except KeyError:
+            sys.exit("Error: IMAGE_DIR environment variable is not set.")
+        
+        # Get random image from picsum.photos    
+        image = requests.get(f"https://picsum.photos/1920/1080").content
+            
+        with open(image_path, "wb") as image_file:
+            image_file.write(image)
+            
         caption = f"Slide nr. {i}!"
         start_date = now + timedelta(days=random.randint(-10, 10))
         end_date = start_date + timedelta(days=random.randint(1, 10))
         active = random.choice([True, False])
-        filetype = random.choice(filetypes)
         
-        slides.append((slide_id, caption, start_date, end_date, active, filetype))
+        slides.append((slide_id, caption, start_date, end_date, active, "jpg"))
     
     cursor.executemany("""
         INSERT INTO slides (id, caption, start_date, end_date, active, filetype)
