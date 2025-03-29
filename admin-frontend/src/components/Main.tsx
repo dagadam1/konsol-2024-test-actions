73a40b964fe9550709b9dba0525a19d04ca5bc28
Adam Olsson <adaolsso@fysiksektionen.se>
2025-03-29 17:26:15 +0100

    Add Google login button

--- admin-frontend/src/components/Main.tsx
+++ admin-frontend/src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SlidesView from './SlidesView';
 import '../styles/Main.css';
 import NewSlide from './NewSlide';
+import { GoogleLogin } from '@react-oauth/google';
 
 const Main: React.FC = () => {
     return (
@@ -12,8 +13,29 @@ const Main: React.FC = () => {
                 </div>
             </div>
             <h1>Konsol</h1>
-            <SlidesView />
-        </>
+
+        <GoogleLogin 
+            onSuccess={credentialResponse => {
+                console.log(credentialResponse);
+                var body: { id_token: any, client_id: any } = { id_token: credentialResponse.credential, client_id: credentialResponse.clientId };
+                fetch(`http://localhost:8080/api/auth/verify`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(body),
+                }).then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.error(err);
+                });
+            }}
+                
+            onError={() => {
+                console.log('Login Failed');
+            }}
+            useOneTap/>;
+    </>
     );
 };
 
--- admin-frontend/src/main.tsx
+++ admin-frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="173515171070-d9anv12crq1ttobl9vsaaao6l7vlr7ei.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
