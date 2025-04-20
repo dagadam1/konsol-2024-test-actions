a1deec5bc405c69cdd9119394fe0e1888c1715bf
Adam Olsson <dagadamsven@gmail.com>
2025-04-20 17:24:29 +0200

    Add unit tests to actions.rs

--- backend/src/actions.rs
+++ backend/src/actions.rs
@@ -60,3 +60,88 @@ pub fn update_slide(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::NaiveDateTime;
+
+    use super::*;
+
+    fn new_test_slide() -> Slide {
+        Slide {
+            id: Uuid::new_v4().to_string(),
+            caption: "Test Slide".to_string(),
+            start_date: NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S")
+                .expect("Invalid datetime"),
+            end_date: NaiveDateTime::parse_from_str("2015-09-08 20:01:04", "%Y-%m-%d %H:%M:%S")
+                .expect("Invalid datetime"),
+            active: true,
+            filetype: "png".to_string(),
+        }
+    }
+
+    fn init_test_db() -> SqliteConnection {
+        let mut conn = SqliteConnection::establish(":memory:").unwrap();
+        let slides_migration = 
+            include_str!("../migrations/2024-12-12-181858_create_slides/up.sql");
+        diesel::sql_query(slides_migration).execute(&mut conn).unwrap();
+        conn
+    }
+
+    #[test]
+    fn test_get_all_slides() {
+        let mut conn = init_test_db();
+        let slides = get_all_slides(&mut conn).unwrap();
+        assert!(slides.is_empty());
+    }
+
+    #[test]
+    fn test_insert_slide() {
+        let mut conn = init_test_db();
+        let slide = new_test_slide();
+
+        let inserted_slide = insert_slide(&mut conn, slide.clone()).unwrap();
+        assert_eq!(inserted_slide, slide);
+
+        let slides = get_all_slides(&mut conn).unwrap();
+        assert_eq!(slides.len(), 1);
+        assert_eq!(slides[0], slide);
+    }
+
+    #[test]
+    fn test_pop_slide() {
+        let mut conn = init_test_db();
+        let slide = new_test_slide();
+
+        insert_slide(&mut conn, slide.clone()).unwrap();
+        let popped_slide = pop_slide(&mut conn, &Uuid::parse_str(&slide.id).unwrap()).unwrap();
+        assert_eq!(popped_slide, slide);
+
+        let slides = get_all_slides(&mut conn).unwrap();
+        assert!(slides.is_empty());
+    }
+
+    #[test]
+    fn test_update_slide() {
+        let mut conn = init_test_db();
+        let mut slide = new_test_slide();
+
+        insert_slide(&mut conn, slide.clone()).unwrap();
+
+        let update_slide_struct = models::UpdateSlide {
+            id: &slide.id,
+            caption: Some("New Title"),
+            start_date: None,
+            end_date: None,
+            active: None,
+            filetype: None,
+        };
+
+        update_slide(&mut conn, update_slide_struct).unwrap();
+
+        let updated_slide = get_all_slides(&mut conn).unwrap().pop().unwrap();
+        slide.caption = "New Title".to_string();
+        
+        assert_eq!(updated_slide, slide);
+    }
+}
