53d2ccda0ac8a927f486926bc0bcada5aa61cd35
Adam Olsson <dagadamsven@gmail.com>
2025-06-16 12:25:08 +0200

    Move SlidesView to SlidesPage

--- admin-frontend/src/pages/SlidesPage.tsx
+++ admin-frontend/src/pages/SlidesPage.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { SlideData } from '../types';
+import Slide from '../components/Slide';
+import '../styles/SlidePage.css';
 
 const SlidesPage: React.FC = () => {
+      const [slides, setSlides] = useState<SlideData[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/screen/slides')
+            .then(response => response.json())
+            .then(json => setSlides(json));
+        setSlides([{ id: 'dummy-id', caption: 'Dummy Slide', start_date: new Date(), end_date: new Date(), active: true, filetype: 'image/png' }, { id: 'dummy-id2', caption: 'Dummy Slide', start_date: new Date(), end_date: new Date(), active: true, filetype: 'image/png' }, { id: 'dummy-id3', caption: 'Dummy Slide', start_date: new Date(), end_date: new Date(), active: true, filetype: 'image/png' }]);
+        console.log(slides);
+    }, []);
+
     return (
-        <div>
-            <h1>Slides Page</h1>
+        <div className='slides-page'>
+            <h1>Slides</h1>
+            <div className='slides'>
+                {slides.map(slide => (
+                    <Slide slide={slide} setSlides={setSlides}/>
+                ))}
+            </div>
         </div>
     );
 };
