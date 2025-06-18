4325648381837fce3a2370a4b639585f6de58a69
Adam Olsson <dagadamsven@gmail.com>
2025-06-18 15:53:30 +0200

    Add routing

--- admin-frontend/src/App.tsx
+++ admin-frontend/src/App.tsx
@@ -1,14 +1,21 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import Header from './components/Header'
 import NavHeader from './components/NavHeader'
 import SlidesPage from './pages/SlidesPage'
 import './styles/App.css'
+import UsersPage from './pages/UsersPage'
 
 function App() {
   return (
     <div className="app-container">
       <Header />
       <NavHeader />
-      <SlidesPage />
+
+      <Routes>
+        <Route path="/" element={<SlidesPage />} />
+        <Route path="/slides" element={<SlidesPage />} />
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
     </div>
   )
 }
