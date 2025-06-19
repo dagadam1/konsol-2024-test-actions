cf55892d8e58182bccecf696329766df0ef0679d
Adam Olsson <dagadamsven@gmail.com>
2025-06-19 17:03:33 +0200

    Add add user button and popup

--- admin-frontend/src/components/User.tsx
+++ admin-frontend/src/components/User.tsx
@@ -11,7 +11,6 @@ const User = (props: Props) => {
         <div className='user'>
             <p>{props.userData.email}</p>
             <p>Admin: {props.userData.admin ? 'Yes' : 'No'}</p>
-            <button className = 'edit-button' onClick={() => alert('Edit user functionality not implemented yet')}>Edit</button>
             <button className = 'delete-button' onClick={() => alert('Delete user functionality not implemented yet')}>Delete</button>
         </div>
   )
--- admin-frontend/src/pages/UsersPage.tsx
+++ admin-frontend/src/pages/UsersPage.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import User from '../components/User';
 import { UserData } from '../types';
 import '../styles/UsersPage.css';
@@ -9,21 +11,65 @@ const UsersPage = (props: Props) => {
     const [users, setUsers] = useState<UserData[]>([]);
 
     useEffect(() => {
-        // fetch('http://localhost:8080/api/auth/list_users')
-        //     .then(response => response.json())
-        //     .then(json => setUsers(json));
-        setUsers([{ id: 'dummy-id', email: 'user1@example.com', admin: true }, { id: 'dummy-id2', email: 'user2@example.com', admin: false }, { id: 'dummy-id3', email: 'user3@example.com', admin: true }]);
+        fetch('http://localhost:8080/api/auth/list_users')
+            .then(response => response.json())
+            .then(json => setUsers(json));
+        // setUsers([{ id: 'dummy-id', email: 'user1@example.com', admin: true }, { id: 'dummy-id2', email: 'user2@example.com', admin: false }, { id: 'dummy-id3', email: 'user3@example.com', admin: true }]);
         console.log(users);
     }, []);
+
+    const handleAddUser = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      console.log(data);
+      const email = data.get('email') as string;
+      const admin = data.get('admin') === 'on';
+
+      if (users.some(user => user.email === email)) {
+        alert('User already exists');
+        return;
+      }
+
+      const newUser = { email, permission: admin ? 'Admin' : 'User' };
+
+      fetch('http://localhost:8080/api/auth/add_user', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newUser),
+      }).then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to add user');
+          }
+          return response.json();
+        }).then(addedUser => {
+          console.log('Added user:', addedUser);
+          setUsers([...users, addedUser]);
+        });
+      }
 
 
   return (
     <div className='users-page'>
+
       <div className="users-header">
         <h1>Users</h1>
-        <button onClick={() => alert('Add user functionality not implemented yet')}>Add User</button>
+        <Popup trigger={<button className='add-user-button'>Add User</button>} modal>
+          <div className='add-user-popup'>
+            <h2>Add User</h2>
+            <form onSubmit={(event) => handleAddUser(event)}>
+              <label>Email:</label>
+              <input type="email" name="email" required />
+              <label>Admin:</label>
+              <input type="checkbox" name="admin" />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </Popup>
+
       </div>
-      {users.map(user => <User userData = {user}/>)}  
+      {users.map(user => <User userData = {user} key={user.id} />)}  
     </div>
   )
 }
