cc78bcb8a64a868d04239553a138ae5d4a3f134e
Adam Olsson <dagadamsven@gmail.com>
2025-08-11 18:48:07 +0200

    Add slide upload to slide page (ugly atm)

--- admin-frontend/src/pages/SlidesPage.tsx
+++ admin-frontend/src/pages/SlidesPage.tsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from 'react';
 import { SlideData } from '../types';
 import Slide from '../components/Slide';
 import '../styles/SlidesPage.css';
+import Popup from 'reactjs-popup';
 
-const SlidesPage: React.FC = () => {
-      const [slides, setSlides] = useState<SlideData[]>([]);
+const SlidesPage = () => {
+
+    const [slides, setSlides] = useState<SlideData[]>([]);
 
     useEffect(() => {
         fetch('http://localhost:8080/api/screen/slides', {
@@ -18,11 +20,53 @@ const SlidesPage: React.FC = () => {
         console.log(slides);
     }, []);
 
+    const handleAddSlide = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+
+        // Visible is an empty string if the checkbox is not checked; we want it to be false instead
+        data.set('visible', data.get('visible') || 'false');
+
+        console.log(data);
+
+        fetch('http://localhost:8080/api/screen/slides/save', {
+            method: 'POST',
+            body: data
+        }).then(response => {
+            if (response.ok) {
+                console.log('Slide saved');
+                //TODO update slides when a new slide is added
+            } else {
+                console.log('Failed to save slide');
+            }
+        });
+    };
+
     return (
         <div className='slides-page'>
+            <div className="slides-header">
+                <h1>Slides</h1>
+                <Popup trigger={<button className='add-slide-button'>Add Slide</button>} modal>
+                    <form onSubmit={(event) => handleAddSlide(event)}>
+                        <button type='submit'>Submit</button>
+                        <label htmlFor='caption'>Caption</label>
+                        <input type='text' id='caption' name='caption' />
+                        <label htmlFor='startDate'>Start Date</label>
+                        <input type='date' id='startDate' name='start' />
+                        <label htmlFor='endDate'>End Date</label>
+                        <input type='date' id='endDate' name='end' />
+                        <label htmlFor='active'>Active</label>
+                        <input type='checkbox' id='active' name='visible' value='true' />
+                        <label htmlFor='file'>File</label>
+                        <input type='file' id='file' name='imageFile' />
+
+                    </form>
+                </Popup>
+
+            </div>
             <div className='slides'>
                 {slides.map(slide => (
-                    <Slide slide={slide} setSlides={setSlides}/>
+                    <Slide slide={slide} setSlides={setSlides} />
                 ))}
             </div>
         </div>
