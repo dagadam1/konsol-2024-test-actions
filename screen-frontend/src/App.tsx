c9acc7440f57e27543e16ffa5d0084f39159fff2
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2025-09-02 10:41:40 +0200

    Add Google Calendar embed Links to the Fysiksektionen Google Calendar. Styled to dark mode with an ad hoc css filter.

--- screen-frontend/src/App.tsx
+++ screen-frontend/src/App.tsx
@@ -16,8 +16,12 @@ function App() {
         <div className='sl'>
           <p>Sl-placeholder</p>
         </div>
-        <div>
-          <p>Calendar placeholder</p>
+        <div className="calendar-placeholder">
+          <iframe
+            className="calendar-frame"
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FStockholm&showPrint=0&showNav=0&mode=AGENDA&hl=sv&title=Fysiksektionens%20kalender&showTabs=0&showCalendars=0&showTz=0&showDate=0&src=ZnlzaWtzZWt0aW9uZW4uc2VfMDE4N3ZibWRjaXZsOG10aW8xNDJlMjNjYXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23FF642B"
+            scrolling="no">
+          </iframe>
         </div>
       </div>
     </>
